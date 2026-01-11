e73a5a33f09cce9001014a0127b283afbe796b60
windihotspot <boiwindihotspot@gmail.com>
2026-01-11 23:07:29 +0100

    responsive header

--- src/layouts/full/MainLayout.vue
+++ src/layouts/full/MainLayout.vue
@@ -26,12 +26,13 @@ onMounted(() => {
 </v-btn>
 
 
-      <img src="/src/assets/New Logo_with_Paratus.png" alt="Logo" class="w-24 h-24 mb-2" />
+      <img src="/src/assets/New Logo_with_Paratus.png" alt="Logo" class="w-24 h-24 mb-2 hidden md:flex" />
 
-      <div class="logo pa-4 flex items-center space-x-2">
-        <p class="font-semibold text-lg">Paratus</p>
-        <span class="text-sm text-gray-600">Powered By: Uppertech One Ltd</span>
-      </div>
+      <div class="logo pa-4 hidden md:flex items-center space-x-2">
+  <p class="font-semibold text-lg">Paratus</p>
+  <span class="text-sm text-gray-600">Powered By: Uppertech One Ltd</span>
+</div>
+
 
       <v-spacer></v-spacer>
       <HeaderView />
